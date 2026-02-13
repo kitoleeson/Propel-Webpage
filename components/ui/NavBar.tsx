87cd6e353ed341ba338124fb025e1988df611d32
Kito <kleeson@ualberta.ca>
2026-02-13 10:43:49 -0700

    updating global.css and encapsulating nav-link and nav-icon styles

--- components/ui/NavBar.tsx
+++ components/ui/NavBar.tsx
@@ -3,16 +3,16 @@ import IconLink from './IconLink'
 
 const NavBar = () => {
   return (
-    <div className='flex items-center justify-between w-full py-4'>
+    <div className='flex items-center justify-between w-full p-2'>
       <div className='flex items-center'>
-        <IconLink iconName='instagram' href='https://www.instagram.com' label='Instagram' className="text-blue-600 hover:text-blue-300" />
-        <IconLink iconName='email' href='mailto:propeltutoringyeg@gmail.com' label='Email' className="text-blue-600 hover:text-blue-300" />
+        <IconLink iconName='instagram' href='https://www.instagram.com' label='Instagram' className="nav-icon" />
+        <IconLink iconName='email' href='mailto:propeltutoringyeg@gmail.com' label='Email' className="nav-icon" />
       </div>
       <div className='flex items-center gap-4'>
-        <Link className="text-blue-600 hover:text-blue-300 p-2 transition-transform duration-200 hover:scale-110"  href="/">HOME</Link>
-        <Link className="text-blue-600 hover:text-blue-300 p-2 transition-transform duration-200 hover:scale-110"  href="/about">ABOUT</Link>
-        <Link className="text-blue-600 hover:text-blue-300 p-2 transition-transform duration-200 hover:scale-110"  href="/signup">SIGN UP</Link>
-        <Link className="text-blue-600 hover:text-blue-300 p-2 transition-transform duration-200 hover:scale-110"  href="/support">SUPPORT</Link>
+        <Link className="nav-link"  href="/">HOME</Link>
+        <Link className="nav-link"  href="/about">ABOUT</Link>
+        <Link className="nav-link"  href="/signup">SIGN UP</Link>
+        <Link className="nav-link"  href="/support">SUPPORT</Link>
       </div>
     </div>
   )
